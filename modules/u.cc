137eb3aa626148b81cfcedf6abe419f66ac894d5
win10_ecust <null>
2016-10-01 11:38:24 +0800

    modi u.cc

--- modules/u.cc
+++ modules/u.cc
@@ -12,21 +12,18 @@
 #include<cstring>
 #include<utility>
 #include<cmath>
-#include<cassert>
 const int inf=0x7fffffff;
 #define IF if(test)
 const bool test=1;
 typedef long long int ll;
 using namespace std;
 typedef pair<int,int> point;
-void pA(int *begin,int n){
-	for(int i=0;i<n;i++){
-		printf("%d " ,*(begin+i));
-	}
-	printf("\n");
+void pA(int *begin,int n){ for(int i=0;i<n;i++){ printf("%d " ,*(begin+i)); } printf("\n"); }
+void sol(){
+
 }
 int main()
 {
-	
+	sol();
 	return 0;
 }
