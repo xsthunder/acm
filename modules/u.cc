8231c5b302f9aee699b431e487186d4e25de9d25
win10_ecust <null>
2017-06-18 10:39:40 +0800

    zoj3329

--- modules/u.cc
+++ modules/u.cc
@@ -2,8 +2,9 @@
 #include<cstdlib>
 #include<cstring>
 #include<iomanip>
+#include<utility>
 using namespace std;
-#define iF if(Te)
+#define IF if(Te)
 #define MS(m) memset(m,0,sizeof(m))
 #ifdef XS
 	#include</home/xs/acm/modules/mylib/De>
@@ -14,6 +15,7 @@ using namespace std;
 
 typedef unsigned U;
 typedef pair<int,int > P;
+typedef long long ll;
 void inp();
 int main(){
 	ios_base::sync_with_stdio(0);
