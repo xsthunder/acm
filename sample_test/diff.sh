c39afe470ab4d40d773ad9fe9d888296d8215fb6
win10_ecust <null>
2017-07-20 08:47:58 +0800

    idff

--- sample_test/diff.sh
+++ sample_test/diff.sh
@@ -3,9 +3,9 @@ g++ -std=c++11 sam.cpp -o sam.exe
 g++ -std=c++11 my.cpp -o my.exe
 while true;do
 	python3 ./py.py>input
-	./sam <input >output.sam
+	./sam.exe <input >output.sam
 	if [ $? -ne 0 ] ; then break; fi
-	./my <input >output.my
+	./my.exe <input >output.my
 	if [ $? -ne 0 ] ; then break; fi
 	diff output.my output.sam 
 	if [ $? -ne 0 ] ; then break; fi
