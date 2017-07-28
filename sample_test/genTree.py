9a0357dc79f0314a9d850728e686da63ea979d26
win10_ecust <null>
2017-07-28 11:02:08 +0800

    genTree

---
+++ sample_test/genTree.py
@@ -0,0 +1,26 @@
+import random
+r = random.randint
+t = 3
+M = 10
+N = 10
+A = 10
+D = 10
+
+def p(x):
+    if(type(x)==type(1)):
+        x = str(x)
+    print (x,sep='',end='')
+def sam():
+    version = 0;
+    n = r(1,N)
+    p(n)
+    p('\n')
+    for i in range(2,n+1):
+        p(i)
+        p(' ')
+        p(r(1,i-1))
+        p('\n')
+    p('\n')
+for i in range(0,3):
+    sam()
+    p('\n')
