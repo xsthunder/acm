33913195fb0c4a6af44fed58a61bf218f6a87bd4
win10_ecust <null>
2017-08-22 21:15:58 +0800

    dif

--- sample_test/diff.sh
+++ sample_test/diff.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 g++ -std=c++11 sam.cc -o sam.out
-g++ -std=c++11 my.cc -o my.out
+g++ -std=c++11 my.cc -o my.outt 
 echo "finish build"
 while true;do
 	python3 ./py.py>input
 	echo "end input gen"
-	time ./sam.exe 
-	#./sam.exe <input >output.sam
+	time ./sam.out 
+	#./sam.out <input >output.sam
 	if [ $? -ne 0 ] ; then break; fi
-	time ./my.exe 
-	#./sam.exe <input >output.my
+	time ./my.out 
+	#./sam.out <input >output.my
 	if [ $? -ne 0 ] ; then break; fi
 	diff output.my output.sam >log.diff
 	#diff output.my output.sam 
