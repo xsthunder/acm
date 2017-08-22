a2399b323cacd705e6401bfa47e64d52da730a5d
win10_ecust <null>
2017-08-22 21:19:52 +0800

    diff /muy

--- sample_test/diff.sh
+++ sample_test/diff.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 g++ -std=c++11 sam.cc -o sam.out
-g++ -std=c++11 my.cc -o my.outt 
+g++ -std=c++11 my.cc -o my.out 
 echo "finish build"
 while true;do
 	python3 ./py.py>input
 	echo "end input gen"
-	#./sam.out <input >output.my
+
+	#./my.out <input >output.my
+	time ./my.out 
 	if [ $? -ne 0 ] ; then break; fi
-	diff output.my output.sam >log.diff
+
 	time ./sam.out 
 	#./sam.out <input >output.sam
 	if [ $? -ne 0 ] ; then break; fi
-	time ./my.out 
+
 	#diff output.my output.sam 
+	diff output.my output.sam >log.diff
 	if [ $? -ne 0 ] ; then break; fi
 done
