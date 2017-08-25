5516a4a0d4fcbfbd7df7dbb773d2506967ec0028
win10_ecust <null>
2017-08-25 13:14:54 +0800

    gaussForInt & qpMatrix upd for negative numbers

--- lib/qpMatrix.cc
+++ lib/qpMatrix.cc
@@ -10,13 +10,14 @@ int main(){
 #ifdef XS
 	freopen("qpMatrix.in","r",stdin);
 #endif
-	while(1)inp();
+	int n;scanf("%d",&n);
+	for(int i=0;i<n;i++)inp();
 	return 0;
 }
 namespace qpMatrix{
-	const int D = 2;
+	const int D = 3;
 	typedef ll M[D][D];
-	ll mod = 1024;
+	ll mod = 1000000007;
 #define MCP(x,y) memcpy(x,y,sizeof(M)) //x=y
 	void pM(M m){
 		for(int i =0;i<D;i++){
@@ -33,6 +34,7 @@ namespace qpMatrix{
 				for(int k=0;k<D;k++){
 					ans[i][j]+=a[i][k]*b[k][j]%mod;
 				}
+				while(ans[i][j]<0)ans[i][j]+=mod;//如果矩阵出现负数
 			}
 		}
 		MCP(m,ans);
@@ -49,18 +51,23 @@ namespace qpMatrix{
 }
 void inp(){
 	using namespace qpMatrix;
-	int a,b,n;if(scanf("%d%d%d%lld",&a,&b,&n,&mod)==EOF)exit(0);
+	ll n;if(scanf("%lld",&n)==EOF)exit(0);
+	mod=1024;
 	M A0={
-		0,a,
-		0,1
+		0,0,9,
+		0,0,97,
+		0,0,969
 	};
+	if(n<=3){ printf("%lld\n",A0[n-1][2]);return; }
+	n-=3;
 	M A={
-		a,b,
-		1,a
+		0,1,0,
+		0,0,1,
+		1,-11,11
 	};
-	qp(A,n-1);
+	qp(A,n);
 	MtM(A,A,A0);
-	printf("%lld\n",(2*A[0][1])%mod);
+	printf("%lld\n",(A[2][2])%mod);
 }
 //qpMatrix.cc by xsthunder at Wed Aug 23 16:21:51 2017
 
