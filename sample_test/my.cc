b292564d838e2ecfdd837d0b6cbbb696c390a6c5
win10_ecust <null>
2017-09-14 22:51:10 +0800

    A

--- sample_test/my.cc
+++ sample_test/my.cc
@@ -1,179 +1,56 @@
-#include<cstdio>
-#include<algorithm>
-#include<cstdlib>
-#include<cstring>
-#include<utility>
-#include<cassert>
+
+#include <bits/stdc++.h>
 using namespace std;
-#define MS(m) memset(m,0,sizeof(m))
+const int N = 400;
+int a[N], b[N], s_a[N], s_b[N];
+char str[N];
+int len, cnt, ans;
+int Max[N];
 
-typedef unsigned U; typedef pair<int,int > P; typedef long long ll;
-void inp();
-int main(){
-#ifdef XS
-    freopen("input","r",stdin);
-#else 
-	freopen("input","r",stdin);
-	freopen("output.my","w",stdout);
-#endif
-		while(1) inp();
-    return 0;
-}
-const int N = 100000 + 1000;
-struct Ed{ int f,t,n; }E[N*2]; int head[N]; int n,Ecnt;
-struct Nod{int l,r;ll s;}nod[30*N];int nodcnt; int root[N];
-inline void ae(int f,int t){
-    E[Ecnt].f = f; E[Ecnt].t = t; E[Ecnt].n = head[f]; head[f] = Ecnt;
-    Ecnt++;
-}
-int siz[N],dep[N],son[N],fa[N];
-void dfs1(int rt,int ff, int d){
-    int w = 0,i,u; siz[rt] = 1; fa[rt] = ff; dep[rt] = d; son[rt] = 0;
-    for(i = head[rt]; i!= -1; i = E[i].n){
-        u = E[i].t;
-        if(u == fa[rt])continue;
-        dfs1(u ,rt, d +1 );
-        siz[rt] += siz[u];
-        if(siz[u] > w){
-            son[rt] = u; w = siz[u];
-        }
-    }
-}
-int top[N],id[N],idcnt;
-void dfs2(int rt ,int to){
-    top[rt] = to; id[rt]=++idcnt ;
-    int u,i;
-    if(son[rt])dfs2(son[rt], to);
-    for(i = head[rt]; i!= -1; i = E[i].n){
-        u = E[i].t;
-        if(u == fa[rt] || u == son[rt])continue;
-        dfs2(u,u);
-    }
-}
-#define lson(z) z<<1
-#define rson(z) (z<<1)+1
-#define mid int m = (L+R)>>1;
-#define pl L, m
-#define pr m+1, R
-#define pushup(z) ma[z] = max(ma[lson(z)], ma[rson(z)])
-int val[N];
-ll que(int rt,int l,int r,int L,int R){
-	if(l<=L&&R<=r)return nod[rt].s;
-	if(r<L||R<l)return 0;
-	mid;
-	return que(nod[rt].l,l,r,pl)
-		+que(nod[rt].r,l,r,pr);
-}
-int unq[N*3];
-int rid[N],unqsize;
-int getvalid(int x){
-	int *it=lower_bound(unq+1,unq+1+unqsize,x);
-	assert(*it=x);
-	return it-unq;
-}
-ll Q(int u,int v,int a,int b){
-	if(dep[v]+dep[u]<300){
-		ll ans=0;
-		int x=u,y=v;
-		while(x!=y){
-			if(dep[x]<dep[y])swap(x,y);
-			int v=val[x];
-			if(a<=v&&v<=b)ans+=v;
-			x=fa[x];
+void dfs(int now, int st, int ret) {
+	if(now >= cnt + 1) {
+		ans = max(ans, ret);
+		return;
+	}
+	for(int i=st;i + b[now] <= len + 1;i++) {
+		int ed = i + b[now];
+		
+		int left = len - ed + 1;
+		if(s_b[cnt] - s_b[now] > left)continue;
+		
+		int tmp = ret + s_a[ed - 1] - s_a[i - 1];
+		if(now >= 2) {
+			if(tmp <= Max[now])continue;
+			else Max[now] = tmp;
 		}
-		int v=val[x];
-		if(a<=v&&v<=b)ans+=v;
-		return ans;
+		//if(now == 2)cout<<ed - 1<<" "<<i - 1<<endl;
+		//if(now == 1)cout<<tmp<<endl;
+		dfs(now + 1, ed, tmp);
 	}
-	ll ans = 0;
-	a=getvalid(a),b=getvalid(b);
-	while(top[u] !=  top[v]){
-		if(dep[top[u]] < dep[top[v]])swap(u,v);
-		ans += que(root[id[u]],a,b,1,unqsize);
-		u = fa[top[u]];
-	}
-	if(dep[u] < dep[v])swap(u,v);
-	ans += que(root[id[u]],a,b,1,unqsize);
-	if(top[v]!=v&&top[fa[v]]==top[v])ans-=que(root[id[fa[v]]],a,b,1,unqsize);
-	return ans;
 }
-struct Query{
-	int x,y,a,b;
-	void set(int x,int y,int a,int b){this->x=x,this->y=y,this->a=a,this->b=b;}
-}queries[N];
-void sol(int m){
-	int u,v,a,b;
-	for(int i =1;i<=m;i++){
-		u=queries[i].x,v=queries[i].y,a=queries[i].a,b=queries[i].b;
-		ll ans = Q(u, v,a,b);
-//if(i!=1)putchar(' ');
-		printf("%lld\n",ans);
+int main() {
+//	freopen("in.txt","r",stdin);
+	while(cin.getline(str + 1, 300, '\n')) {
+		memset(s_a, 0, sizeof(s_a));
+		memset(s_b, 0, sizeof(s_b));
+		memset(Max, -1, sizeof(Max));
+		ans = 0;
+		
+		
+		len = strlen(str + 1);
+		//cout<<str+1<<" "<<len<<endl;
+		for(int i=1;i<=len;i++)a[i] = str[i] - '0';
+		for(int i=1;i<=len;i++)s_a[i] = s_a[i - 1] + a[i];
+		
+		cin.getline(str + 1, 300, '\n'); 
+		cnt = strlen(str + 1);
+		//cout<<str+1<<" "<<cnt<<endl;
+		for(int i=1;i<=cnt;i++)b[i] = str[i] - 'a' + 1;
+		for(int i=1;i<=cnt;i++)s_b[i] = s_b[i - 1] + b[i];
+		//cout<<s_b[cnt]<<endl;
+		dfs(1, 1, 0);
+		
+		cout<<ans<<endl;
 	}
-	putchar('\n');
+	return 0;
 }
-void bui(int &rt,int val,int id,int L,int R){
-	nod[nodcnt]=nod[rt];
-	rt=nodcnt;nodcnt++;
-	nod[rt].s+=val;
-	if(L==R)return ;
-	mid;
-	if(id<=m){
-		bui(nod[rt].l,val,id,pl);
-	}
-	else bui(nod[rt].r,val,id,pr);
-}
-
-void pcmt(int rt,int w = 0){
-#define get(z) nod[z]
-	if(get(rt).r!=0)pcmt(get(rt).r, w+10);
-	printf("%*sn[%d]l:%dr:%ds:%lld\n", w,"", rt,get(rt).l,get(rt).r,get(rt).s);
-	if(get(rt).l!=0)pcmt(get(rt).l, w+10);
-}
-void initroot(){
-	top[0]=0;rid[0]=0;
-	int v=0,vid;
-	for(int i =1;i<=n;i++)rid[id[i]]=i;
-	for(int i =1;i<=n;i++){
-		if(top[rid[i-1]]==top[rid[i]]){ root[i]=root[i-1]; }
-		else root[i]=0;
-		v = val[rid[i]]; vid=getvalid(v);
-		bui(root[i],v,vid,1,unqsize);
-	}
-	return ;
-#ifdef XS
-	for(int i=1;i<=n;i++){
-		printf("----vvvvvvvvvvroot[%d]\n",i);
-		pcmt(root[i]);
-	}
-#endif
-}
-
-void inp(){
-	memset(head,-1,sizeof(head));Ecnt = 0;idcnt = 0;
-	MS(top),MS(dep),MS(E),MS(son),MS(val),MS(fa),MS(id),MS(siz);
-	MS(nod),nodcnt=1,MS(root);
-	int m;if(scanf("%d%d",&n,&m)==EOF)exit(0);
-	if(!n&&!m)exit(0);
-	for(int i =1;i<=n;i++)scanf("%d",&val[i]);
-	int u,v;
-	for(int i =1;i<=n-1;i++){ scanf("%d%d",&u,&v);
-		ae(u,v); ae(v,u); }
-	memcpy(unq,val,sizeof(val));
-	int x,y,a,b;
-	unqsize=n;
-	for(int i=1;i<=m;i++){
-		scanf("%d%d%d%d",&x,&y,&a,&b);
-		queries[i].set(x,y,a,b);
-		unq[++unqsize]=a;
-		unq[++unqsize]=b;
-	}
-	sort(unq+1,unq+1+unqsize);
-	unqsize=unique(unq+1,unq+1+unqsize)-(unq+1);
-	int rt = 1,de = 1;
-	dfs1(rt, 0, de);
-	dfs2(rt, rt);
-	initroot();
-	sol(m);
-	exit(0);
-}
-
--- sample_test/sam.cc
+++ sample_test/sam.cc
@@ -1,165 +1,60 @@
 #include<cstdio>
 #include<algorithm>
+#include<cassert>
 #include<cstdlib>
 #include<cstring>
 #include<utility>
-#include<cassert>
 using namespace std;
-#define MS(m) memset(m,0,sizeof(m))
-
+#define MS(m,z) memset(m,z,sizeof(m))
 typedef unsigned U; typedef pair<int,int > P; typedef long long ll;
 void inp();
 int main(){
 #ifdef XS
-    //freopen("input","r",stdin);
+	freopen("sam2.in","r",stdin);
 #endif
-#ifdef DIFF
-#endif
-        while(1) inp();
-    return 0;
+	while(1)inp();
+	return 0;
 }
-const int N = 100000 + 1000;
-struct Ed{ int f,t,n; }E[N*2]; int head[N]; int n,Ecnt;
-struct Nod{int l,r;ll s;}nod[30*N];int nodcnt; int root[N];
-inline void ae(int f,int t){
-    E[Ecnt].f = f; E[Ecnt].t = t; E[Ecnt].n = head[f]; head[f] = Ecnt;
-    Ecnt++;
+const int N = 400;
+char s[N],s2[N];
+int len,len2;
+int sum[N],sum2[N];
+int ans;
+void dfs(int d,int p,int r,int res){
+	if(d==len2+1){
+		ans=max(ans,res);
+	}
+	if(p>len)return;
+	if(len-p+1<sum2[len2]-sum2[d-1])return;
+	if(!r){
+		res+=sum[len]-sum[p-1];
+		ans=max(ans,res);
+		return;
+	}
+	res+=sum[p+s2[d]-1]-sum[p-1];
+	p+=s2[d];
+	for(int i=0;i<=r;i++){
+		dfs(d+1,p+i,r-i,res);
+	}
 }
-int siz[N],dep[N],son[N],fa[N];
-void dfs1(int rt,int ff, int d){
-    int w = 0,i,u; siz[rt] = 1; fa[rt] = ff; dep[rt] = d; son[rt] = 0;
-    for(i = head[rt]; i!= -1; i = E[i].n){
-        u = E[i].t;
-        if(u == fa[rt])continue;
-        dfs1(u ,rt, d +1 );
-        siz[rt] += siz[u];
-        if(siz[u] > w){
-            son[rt] = u; w = siz[u];
-        }
-    }
-}
-int top[N],id[N],idcnt;
-void dfs2(int rt ,int to){
-    top[rt] = to; id[rt]=++idcnt ;
-    int u,i;
-    if(son[rt])dfs2(son[rt], to);
-    for(i = head[rt]; i!= -1; i = E[i].n){
-        u = E[i].t;
-        if(u == fa[rt] || u == son[rt])continue;
-        dfs2(u,u);
-    }
-}
-#define lson(z) z<<1
-#define rson(z) (z<<1)+1
-#define mid int m = (L+R)>>1;
-#define pl L, m
-#define pr m+1, R
-#define pushup(z) ma[z] = max(ma[lson(z)], ma[rson(z)])
-int val[N];
-ll que(int rt,int l,int r,int L,int R){
-    if(l<=L&&R<=r)return nod[rt].s;
-    if(r<L||R<l)return 0;
-    mid;
-    return que(nod[rt].l,l,r,pl)
-        +que(nod[rt].r,l,r,pr);
-}
-int unq[N*3];
-int rid[N],unqsize;
-int getvalid(int x){
-    int *it=lower_bound(unq+1,unq+1+unqsize,x);
-    assert(*it=x);
-    return it-unq;
-}
-ll Q(int u,int v,int a,int b){
-    ll ans = 0;
-    a=getvalid(a),b=getvalid(b);
-    while(top[u] !=  top[v]){
-        if(dep[top[u]] < dep[top[v]])swap(u,v);
-        ans += que(root[id[u]],a,b,1,unqsize);
-        u = fa[top[u]];
-    }
-    if(dep[u] < dep[v])swap(u,v);
-    ans += que(root[id[u]],a,b,1,unqsize);
-    if(top[v]!=v&&top[fa[v]]==top[v])ans-=que(root[id[fa[v]]],a,b,1,unqsize);
-    return ans;
-}
-struct Query{
-    int x,y,a,b;
-    void set(int x,int y,int a,int b){this->x=x,this->y=y,this->a=a,this->b=b;}
-}queries[N];
-void sol(int m){
-    int u,v,a,b;
-    for(int i =1;i<=m;i++){
-        u=queries[i].x,v=queries[i].y,a=queries[i].a,b=queries[i].b;
-        ll ans = Q(u, v,a,b);
-        if(i!=1)putchar(' ');
-        printf("%lld",ans);
-    }
-    putchar('\n');
-}
-void bui(int &rt,int val,int id,int L,int R){
-    nod[nodcnt]=nod[rt];
-    rt=nodcnt;nodcnt++;
-    nod[rt].s+=val;
-    if(L==R)return ;
-    mid;
-    if(id<=m){
-        bui(nod[rt].l,val,id,pl);
-    }
-    else bui(nod[rt].r,val,id,pr);
-}
-
-void pcmt(int rt,int w = 0){
-#define get(z) nod[z]
-    if(get(rt).r!=0)pcmt(get(rt).r, w+10);
-    printf("%*sn[%d]l:%dr:%ds:%lld\n", w,"", rt,get(rt).l,get(rt).r,get(rt).s);
-    if(get(rt).l!=0)pcmt(get(rt).l, w+10);
-}
-void initroot(){
-    top[0]=0;rid[0]=0;
-    int v=0,vid;
-    for(int i =1;i<=n;i++)rid[id[i]]=i;
-    for(int i =1;i<=n;i++){
-        if(top[rid[i-1]]==top[rid[i]]){ root[i]=root[i-1]; }
-        else root[i]=0;
-        v = val[rid[i]]; vid=getvalid(v);
-        bui(root[i],v,vid,1,unqsize);
-    }
-    return ;
-#ifdef XS
-    for(int i=1;i<=n;i++){
-        printf("----vvvvvvvvvvroot[%d]\n",i);
-        pcmt(root[i]);
-    }
-#endif
-}
-
 void inp(){
-    memset(head,-1,sizeof(head));Ecnt = 0;idcnt = 0;
-    MS(top),MS(dep),MS(E),MS(son),MS(val),MS(fa),MS(id),MS(siz);
-    MS(nod),nodcnt=1,MS(root);
-    int m;if(scanf("%d%d",&n,&m)==EOF)exit(0);
-    if(!n&&!m)exit(0);
-    for(int i =1;i<=n;i++)scanf("%d",&val[i]);
-    int u,v;
-    for(int i =1;i<=n-1;i++){ scanf("%d%d",&u,&v);
-        ae(u,v); ae(v,u); }
-    memcpy(unq,val,sizeof(val));
-    int x,y,a,b;
-    unqsize=n;
-    for(int i=1;i<=m;i++){
-        scanf("%d%d%d%d",&x,&y,&a,&b);
-        queries[i].set(x,y,a,b);
-        unq[++unqsize]=a;
-        unq[++unqsize]=b;
-    }
-    sort(unq+1,unq+1+unqsize);
-    unqsize=unique(unq+1,unq+1+unqsize)-(unq+1);
-    int rt = 1,de = 1;
-    dfs1(rt, 0, de);
-    dfs2(rt, rt);
-    initroot();
-    sol(m);
+	ans=0;
+	if(scanf("%s",s+1)==EOF)exit(0);
+	getchar();
+	assert(s2+1==fgets(s2+1,N,stdin));
+	len=strlen(s+1),len2=strlen(s2+1);len2--;
+	for(int i=1;i<=len;i++)s[i]-='0';
+	for(int i=1;i<=len2;i++)s2[i]-='a',s2[i]++;
+	MS(sum,0);MS(sum2,0);
+	for(int i=1;i<=len;i++)sum[i]=sum[i-1]+s[i];
+	for(int i=1;i<=len2;i++)sum2[i]=sum2[i-1]+s2[i];
+	assert(sum2[len2]<=len);
+	int r = len;
+	for(int i=1;i<=len2;i++)r-=s2[i];
+	if(len2)for(int i=0;i<=r;i++){
+		dfs(1,1+i,r-i,0);
+	}
+	printf("%d\n",ans);
 }
+//sam.cc by xsthunder at Thu Sep 14 21:36:46 2017
 
-//AC at Wed Aug 23 08:02:47 2017
